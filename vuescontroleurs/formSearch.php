c66c1f32eaf6dd8b9b945f0de1bfc1ba2f644b2c
LeLicorne_ <110946219+LeLicorne@users.noreply.github.com>
2023-03-04 18:04:21 +0100

    del - header(location:)

--- vuescontroleurs/formSearch.php
+++ vuescontroleurs/formSearch.php
@@ -7,10 +7,9 @@ $lepdo=connectionBDD();
 include_once "vues/search.php";
 
 if(isset($_POST['submit'])){
+
     $noVille=$_POST['villeList'];
     $noType=$_POST['typeList'];
     getBiensSearch($lepdo, $_POST['villeList'], $_POST['typeList']);
-    
-    header("Location: ?NoVille=$noVille&NoType=$noType");
 }
 ?>
