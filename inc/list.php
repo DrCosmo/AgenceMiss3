a1714f5b436a1d9d074a76c56e250db6b7ed8eb5
Mathéo Castre <59379120+DrCosmo@users.noreply.github.com>
2023-03-12 22:54:48 +0100

    Fix les chemins relatifs
    Les chemins relatifs partaient du fichier "list.php", hors tout ce fait dans la page "index.php".
    Il faut donc faire les chemins relatifs en fonction de "index.php"

--- inc/list.php
+++ inc/list.php
@@ -1,10 +1,10 @@
 <?php
-    include_once '../modeles/fonctionsAccesBDD.php';
+    include_once 'modeles/fonctionsAccesBDD.php';
     $pdo = connectionBDD();
 ?>
     <div id=listerBiens>
 <?php
-    include_once '../modeles/fonctionsAccesBDD.php';
+    include_once 'modeles/fonctionsAccesBDD.php';
     $lesBiens = getToutBiens($pdo);
     foreach ($lesBiens as $unBien){
         echo '<option value="'.'">'.'- '.$unBien['nbpiece'].',un(e) '.$unBien['surface'].' au prix de '.$unBien['prix'].'euros à '.$unBien['ville'].'</option>';
