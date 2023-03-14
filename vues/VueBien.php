012a97e9ff8814c6704b4cc0251571f2488d3a4d
Mathéo Castre <59379120+DrCosmo@users.noreply.github.com>
2023-03-14 02:30:18 +0100

    Add files via upload

---
+++ vues/VueBien.php
@@ -0,0 +1,54 @@
+<?php
+include_once "../inc/header.inc.php";
+include_once '../autres/verifBienReference.php';  
+    $reference = $_GET['reference'];
+    $leBien = getBienByReference($db, $reference);
+?>
+
+<section class="midpage">
+        <div class="top">
+            <div class="topleft">
+                <div class="title">
+                    <h1>
+                    <?php echo $leBien["type_bien"]." ".$leBien["nbpiece"]." pièces";?></h1>
+                
+                    </h1>
+                    <span>
+                    <h3><?php echo $leBien['prix']." €";?></h3>
+                        
+                    </span>
+                </div>
+
+                <div class="caracteristiques">
+                    <section>
+                        <h2>L'essentiel</h2>
+                        <span>2 chambre</span>
+                        <span>1 bureau</span>
+                        <span>Terrain 1 675 m²</span>
+                    </section>
+                    <section>
+                        <h2>L'intérieur</h2>
+                        <span>Cheminée</span>
+                        <span>Climatisation</span>
+                        <span>Alarme</span>
+                    </section>
+                    <section>
+                        <h2>L'extérieur</h2>
+                        <span>Piscine</span>
+                        <span>Terrasse</span>
+                        <span>Jardin</span>
+                    </section>
+                </div>
+                </div>
+                <div class="slider">
+                <img src="../<?php echo $leBien['img']?>" alt="Le bien">
+            </div>
+
+        <article>
+            <h1>
+                Description
+            </h1>
+            <p>
+            <?php echo $leBien['description'];?>
+                            </p>
+        </article>
