937217e27bd9dcd51a879126d7a7d8135c617547
ElCuist0 <124583297+ElCuist0@users.noreply.github.com>
2023-03-14 15:10:05 +0100

    Add files via upload

--- controleurs/formSearch.php
+++ controleurs/formSearch.php
@@ -10,8 +10,11 @@ if(isset($_POST['submit'])){
 
     $noVille=$_POST['villeList'];
     $noType=$_POST['typeList'];
-    getBiensSearch($lepdo, $_POST['villeList'], $_POST['typeList']);
-    header("Location: ?page=lesbiens&Recherche=1&noVille=$noVille&noType=$noType");
+    $noJardin=$_POST['ChoixJardin'];
+    $noPrixMax=$_POST['ChoixPrixMax'];
+    $noPrixMin=$_POST['ChoixPrixMin'];
+    $recherche=getBiensSearch($lepdo, $_POST['villeList'], $_POST['typeList'],$_POST['ChoixJardin'],$_POST['ChoixPrixMax'],$_POST['ChoixPrixMin']);
+    header("Location: ?page=lesbiens&Recherche=1&noVille=$noVille&noType=$noType&noJardin=$noJardin&noPrixMax=$noPrixMax&noPrixMin=$noPrixMin");
 }
 
 if (isset($_POST['reset'])) {
