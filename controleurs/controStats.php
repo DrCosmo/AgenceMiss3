72e0930b104fcafeac5dabda1fe6671c038595f2
LeLicorne_ <lo.re.baptiste.59@gmail.com>
2023-04-10 18:08:52 +0100

    contro de la vue stats a jour pour les prix

--- controleurs/controStats.php
+++ controleurs/controStats.php
@@ -6,3 +6,30 @@ $lepdo=connectionBDD();
 
 include_once "vues/stats.php";
 
+?>
+
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['TranchePrix', 'NbRecherche'],
+          <?php
+            foreach (getStatsPrix($lepdo) as $uneStat) {
+                echo "['".$uneStat['prixMin']."--".$uneStat['prixMax']."',".$uneStat['COUNT(tranchePrix)']."],";
+            }
+          ?>
+        ]);
+
+        var options = {
+          title: ''
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piePrix'));
+
+        chart.draw(data, options);
+      }
+    </script>
--- vues/stats.php
+++ vues/stats.php
@@ -1,7 +1,7 @@
 <div id="statsPage">
     <div class="statsPrix">
         <h2>Statistique de Tranche de prix selectioné :</h2>
-        <div id="piePrix" style="width: 900px; height: 500px;"></div>
+        <div id="piePrix" style="width: 400px; height: 300px"></div>
     </div>
     <div class="statsVille">
         <h2>Statistique de Ville selectioné :</h2>
