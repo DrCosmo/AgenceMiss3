7bb4bb587e6f25950a15a1c481ce3dfcecdf94c2
Mathéo Castre <59379120+DrCosmo@users.noreply.github.com>
2023-04-11 15:06:13 +0200

    Add files via upload

--- tests/testStatsPie.php
+++ tests/testStatsPie.php
@@ -8,9 +8,12 @@ if (isset($_POST['sub'])) {
   $date1=$_POST['date1'];
   $date2=$_POST['date2'];
   $statsPrix=getStatsPrix($lepdo, $date1, $date2);
+  $statsSurface=getSurface($lepdo, $date1, $date2);
   var_dump($statsPrix);
+  var_dump($statsSurface);
 }else {
   $statsPrix=getStatsPrix($lepdo, NULL, NULL);
+  $statsSurface=getSurface($lepdo, NULL, NULL);
 }
 
 ?>
@@ -44,6 +47,29 @@ if (isset($_POST['sub'])) {
 
         var chart = new google.visualization.PieChart(document.getElementById('piePrix'));
 
+        chart.draw(data, options);
+      }
+    </script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['surface', 'Recherches par surface'],
+          <?php
+            foreach ($statsSurface as $uneStat) {
+              echo "['".$uneStat['surface']."',".$uneStat['COUNT(surface)']."],";
+            }
+          ?>
+        ]);
+
+        var options = {
+          curveType: 'function',
+          legend: { position: 'bottom' }
+        };
+
+        var chart = new google.visualization.BarChart(document.getElementById('chartSurface'));
         chart.draw(data, options);
       }
     </script>
