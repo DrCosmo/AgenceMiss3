b455c36398a2669f081568115c3c98a9aa191149
Cosmo <matheo.castre@gmail.com>
2024-02-03 22:32:48 +0100

    ajout de media queries

--- vues/home.php
+++ vues/home.php
@@ -1,5 +1,5 @@
 <div class="hero">
-    <div class="hero-title">
+    <div class="text-section">
         <h1>Trouvez la Maison de Vos Rêves avec Notre Expertise Immobilière</h1>
         <h3>Explorez un monde d'opportunités immobilières exceptionnelles, pour transformer vos rêves immobiliers en réalité.</h3>
         <div class="button">
@@ -11,7 +11,7 @@
 
 <div class="newsletter">
     <img src="img/news" alt="">
-    <div class="hero-title">
+    <div class="text-section">
         <h1>Restez Connecté avec Nos Offres Exclusives!</h1>
         <h3>Ne manquez jamais une opportunité. Abonnez-vous à notre newsletter pour recevoir en avant-première les dernières annonces, les tendances du marché immobilier et des offres exclusives. Soyez informé en temps réel et soyez prêt à saisir les meilleures occasions. L'avenir de votre nouveau bien commence ici.</h3>
         <div class="button">
