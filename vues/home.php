69e378ddbec676ac03f3a31bea97c26ff9c6c1cf
Cosmo <matheo.castre@gmail.com>
2024-02-03 22:18:39 +0100

    home page + fix footer

--- vues/home.php
+++ vues/home.php
@@ -8,3 +8,14 @@
     </div>
     <img src="img/Agences/Agence_M.jpg" alt="">
 </div>
+
+<div class="newsletter">
+    <img src="img/news" alt="">
+    <div class="hero-title">
+        <h1>Restez Connecté avec Nos Offres Exclusives!</h1>
+        <h3>Ne manquez jamais une opportunité. Abonnez-vous à notre newsletter pour recevoir en avant-première les dernières annonces, les tendances du marché immobilier et des offres exclusives. Soyez informé en temps réel et soyez prêt à saisir les meilleures occasions. L'avenir de votre nouveau bien commence ici.</h3>
+        <div class="button">
+            <a href="?page=newsletter">Inscription à la newsletter</a>
+        </div>
+    </div>
+</div>
